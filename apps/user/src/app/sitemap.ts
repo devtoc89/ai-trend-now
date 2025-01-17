981674ea7abe740ad8f943fac39099acdeee7b01
devtoc89 <devtoc89@gmail.com>
2025-01-17 11:21:49 +0900

    feat/user: change sitemap creating logic(only use 10 page of list)

--- apps/user/src/app/sitemap.ts
+++ apps/user/src/app/sitemap.ts
@@ -1,27 +1,18 @@
-import { PostCategoryEnum } from "@repo/types/enums/post.category.enum";
 import { range } from "es-toolkit";
 import type { MetadataRoute } from "next/types";
-import { NEWS_PAGE_SIZE } from "#consts/global";
-import { getPostIdsActionCache, getPostListAndCountActionCache } from "#layer/action/post.action";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { totalCount } = await getPostListAndCountActionCache(PostCategoryEnum.PAPER, 0, NEWS_PAGE_SIZE);
-  const pageCnt = Math.ceil(totalCount / NEWS_PAGE_SIZE);
+  // const { totalCount } = await getPostListAndCountActionCache(PostCategoryEnum.PAPER, 0, NEWS_PAGE_SIZE);
+  // const pageCnt = Math.ceil(totalCount / NEWS_PAGE_SIZE);
+  const pageCnt = 10;
   const sitemapFromPosts: MetadataRoute.Sitemap = range(1, pageCnt + 1).map((page) => {
     return {
       url: `https://ai-trend-now.stream/post/paper/page/${page}`,
       lastModified: new Date(),
-      changeFrequency: "hourly",
-      priority: 0.5,
+      changeFrequency: "always",
+      priority: 1,
     };
   });
-  const sitemapFromPostDetail: MetadataRoute.Sitemap = (await getPostIdsActionCache(PostCategoryEnum.PAPER)).map(
-    (id) => ({
-      url: `https://ai-trend-now.stream/post/paper/${id}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    }),
-  );
-  return [...sitemapFromPostDetail, ...sitemapFromPosts];
+
+  return [...sitemapFromPosts];
 }
