d68daaa360aab133ee25717aca1588f581a82278
devtoc89 <devtoc89@gmail.com>
2025-01-22 15:33:36 +0900

    chore/user: SEO TEST, sitemap을 목록 50개로 변경 및 변경 주기를 always에서 hourly로 변경경

--- apps/user/src/app/sitemap.ts
+++ apps/user/src/app/sitemap.ts
@@ -4,12 +4,12 @@ import type { MetadataRoute } from "next/types";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // const { totalCount } = await getPostListAndCountActionCache(PostCategoryEnum.PAPER, 0, NEWS_PAGE_SIZE);
   // const pageCnt = Math.ceil(totalCount / NEWS_PAGE_SIZE);
-  const pageCnt = 10;
+  const pageCnt = 50;
   const sitemapFromPosts: MetadataRoute.Sitemap = range(1, pageCnt + 1).map((page) => {
     return {
       url: `https://ai-trend-now.stream/post/paper/page/${page}`,
       lastModified: new Date(),
-      changeFrequency: "always",
+      changeFrequency: "hourly",
       priority: 1,
     };
   });
