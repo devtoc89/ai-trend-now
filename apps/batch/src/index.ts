34fde825065c92b2169d810d916409706eebac29
devtoc89 <devtoc89@gmail.com>
2025-02-01 14:10:22 +0900

    fix/manager: 아카이브 특성 상, 과거의 일자로 논문이 발행됨을 감안하여 배치 처리 변경경

--- apps/batch/src/index.ts
+++ apps/batch/src/index.ts
@@ -1,18 +1,21 @@
 require("dotenv").config();
 
-import { jobRegister } from "#helper/job.helper";
-import { arxivAiPostJob } from "#jobs/arxivAiPostJob";
-import { arxivCrawlJob } from "#jobs/arxivCrawlJob";
 import { logger } from "@repo/util/logger/pino.instance";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import { jobRegister } from "#helper/job.helper";
+import { arxivAiPostJob } from "#jobs/arxivAiPostJob";
+import { arxivCrawlJobMaker } from "#jobs/arxivCrawlJob";
 
 dayjs.extend(utc);
 
 function start() {
   logger.info("batch server now is running.");
-  jobRegister(arxivCrawlJob, "0 */15 * * * *");
-  jobRegister(arxivAiPostJob, "0 */5 * * * *", true);
+  // TODO: make cronjob by rule based.
+  for (let i = 0; i <= 31; i++) {
+    jobRegister(arxivCrawlJobMaker({ timeDelta: { unit: "day", value: -i } }), `0 */${i + 1} * * * *`);
+  }
+  jobRegister(arxivAiPostJob, "*/12 * * * * *", true);
 }
 
 start();
--- apps/batch/src/jobs/arxivCrawlJob.ts
+++ apps/batch/src/jobs/arxivCrawlJob.ts
@@ -1,21 +1,35 @@
-import dayjs from "dayjs";
+import dayjs, { type ManipulateType } from "dayjs";
 
-function makeParams() {
+export type ArxivCrawlJobParams = { timeDelta: { unit: ManipulateType; value: number } };
+
+function makeParams({ timeDelta }: ArxivCrawlJobParams) {
   const nowUtc = dayjs().utc();
+  const fromDate = nowUtc.add(timeDelta.value, timeDelta.unit);
+  const toDate = fromDate.add(1, "day");
   return JSON.stringify({
-    from: `${nowUtc.subtract(1, "hour").format("YYYYMMDDHH")}0000`,
-    to: `${nowUtc.add(1, "hour").format("YYYYMMDDHH")}0000`,
+    from: `${fromDate.format("YYYYMMDD")}`,
+    to: `${toDate.format("YYYYMMDD")}`,
     source: "arxiv",
   });
 }
 
-export async function arxivCrawlJob() {
+export async function arxivCrawlJob(params: ArxivCrawlJobParams) {
   const res = await fetch(`${process.env.CRAWLER_SERVER_URL}/crawl`, {
     method: "post",
-    body: makeParams(),
+    body: makeParams(params),
     headers: {
       "Content-Type": "application/json",
     },
   });
   return JSON.stringify(await res.json()).substring(0, 100);
+}
+
+export function arxivCrawlJobMaker(params: ArxivCrawlJobParams) {
+  const targetFn = function () {
+    return arxivCrawlJob(params);
+  };
+  Object.defineProperty(targetFn, "name", {
+    value: `${arxivCrawlJob.name} : ${JSON.stringify(params)}`,
+  });
+  return targetFn;
 }
