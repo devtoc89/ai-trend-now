3fc53f023e337bc2e0b30c756a96eb86342e3e73
devtoc89 <devtoc89@gmail.com>
2025-02-14 13:26:43 +0900

    fix/user: 사이트맵 확장장

--- apps/user/src/app/sitemap.ts
+++ apps/user/src/app/sitemap.ts
@@ -1,21 +1,29 @@
 import { PostCategoryEnum } from "@repo/types/enums/post.category.enum";
 import { range } from "es-toolkit";
+import { unstable_cache } from "next/cache";
 import type { MetadataRoute } from "next/types";
 import { NEWS_PAGE_SIZE } from "#consts/global";
 import { getPostTotalCountAction } from "#layer/action/post.action";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const totalCount = await getPostTotalCountAction({ category: PostCategoryEnum.PAPER });
-  const pageCnt = Math.max(Math.ceil(totalCount / NEWS_PAGE_SIZE), 50000);
+const sitemap = unstable_cache(
+  async () => {
+    const totalCount = await getPostTotalCountAction({ category: PostCategoryEnum.PAPER });
+    const pageCnt = Math.min(Math.ceil(totalCount / NEWS_PAGE_SIZE), 5000);
 
-  const sitemapFromPosts: MetadataRoute.Sitemap = range(1, pageCnt + 1).map((page) => {
-    return {
-      url: `https://ai-trend-now.stream/post/paper/page/${page}`,
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.5,
-    };
-  });
+    const sitemapFromPosts: MetadataRoute.Sitemap = range(1, pageCnt + 1).map((page) => {
+      return {
+        url: `https://ai-trend-now.stream/post/paper/page/${page}`,
+        lastModified: new Date(),
+        changeFrequency: "daily",
+        priority: 0.5,
+      };
+    });
+    return sitemapFromPosts;
+  },
+  ["sitemap"],
+  {
+    revalidate: 3600,
+  },
+);
 
-  return [...sitemapFromPosts];
-}
+export default sitemap;
