a7c88e847a128ade5b3b0362cd68577f2bf1cdd7
devtoc89 <devtoc89@gmail.com>
2025-02-14 13:15:48 +0900

    fix/user: 사이트맵 확장장

--- apps/user/src/app/sitemap.ts
+++ apps/user/src/app/sitemap.ts
@@ -1,16 +1,19 @@
+import { PostCategoryEnum } from "@repo/types/enums/post.category.enum";
 import { range } from "es-toolkit";
 import type { MetadataRoute } from "next/types";
+import { NEWS_PAGE_SIZE } from "#consts/global";
+import { getPostTotalCountAction } from "#layer/action/post.action";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // const { totalCount } = await getPostListAndCountActionCache(PostCategoryEnum.PAPER, 0, NEWS_PAGE_SIZE);
-  // const pageCnt = Math.ceil(totalCount / NEWS_PAGE_SIZE);
-  const pageCnt = 50;
+  const totalCount = await getPostTotalCountAction({ category: PostCategoryEnum.PAPER });
+  const pageCnt = Math.max(Math.ceil(totalCount / NEWS_PAGE_SIZE), 50000);
+
   const sitemapFromPosts: MetadataRoute.Sitemap = range(1, pageCnt + 1).map((page) => {
     return {
       url: `https://ai-trend-now.stream/post/paper/page/${page}`,
       lastModified: new Date(),
-      changeFrequency: "hourly",
-      priority: 1,
+      changeFrequency: "daily",
+      priority: 0.5,
     };
   });
 
