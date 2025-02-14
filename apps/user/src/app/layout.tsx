f0974ea865233347bd523b126f1af7957698ed38
devtoc89 <devtoc89@gmail.com>
2025-02-14 12:47:56 +0900

    fix/user: 루트레이아웃의 메타 삭제제

--- apps/user/src/app/layout.tsx
+++ apps/user/src/app/layout.tsx
@@ -6,16 +6,16 @@ import RootLayer from "#layer/ui/layouts/root/RootLayer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import type { Metadata } from "next/types";
-import { ARTICLE_LIST_PAGE_TITLE, GLOBAL_DESCRIPTION } from "#consts/global";
-import { commonMetadata } from "#lib/helper/metadata/metadata.helper";
-export const metadata: Metadata = await commonMetadata({
-  title: ARTICLE_LIST_PAGE_TITLE,
-  description: GLOBAL_DESCRIPTION,
-  keywords: ["AI 뉴스", "기술 트렌드", "AI 트렌드 요약"].join(", "),
-});
+// import type { Metadata } from "next/types";
+// import { ARTICLE_LIST_PAGE_TITLE, GLOBAL_DESCRIPTION } from "#consts/global";
+// import { commonMetadata } from "#lib/helper/metadata/metadata.helper";
+// export const metadata: Metadata = await commonMetadata({
+//   title: ARTICLE_LIST_PAGE_TITLE,
+//   description: GLOBAL_DESCRIPTION,
+//   keywords: ["AI 뉴스", "기술 트렌드", "AI 트렌드 요약"].join(", "),
+// });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
