9aebdc066d4a450fdfea180310962971db6304c7
devtoc89 <devtoc89@gmail.com>
2025-02-19 20:56:54 +0900

    fix/user:  detail page "use server" directive 추가가

--- apps/user/src/layer/ui/pages/PostDetailPage/(server)/PostDetailPage.tsx
+++ apps/user/src/layer/ui/pages/PostDetailPage/(server)/PostDetailPage.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import { type PostItemViewItem, getPostItemActionCache } from "#layer/action/post.action";
 import NoData from "#layer/ui/components/common/(server)/NoData/NoData";
 import PostDetailPageContent from "#layer/ui/pages/PostDetailPage/(server)/PostDetailPageContent";
